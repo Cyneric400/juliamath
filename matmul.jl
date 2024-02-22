daf40e8019ed430405203168f0b92cc0db7860f1
Cyneric400 <davidrsharpe@southern.edu>
2024-02-22 08:59:10 -0500

    added sources

--- matmul.jl
+++ matmul.jl
@@ -1,14 +1,6 @@
 # First started in MATH-280 class, mid-Feb 2024
 # Getting loops together: 2/20/2024
 
-########
-# Sources:
-# https://www.geeksforgeeks.org/get-array-dimensions-and-size-of-a-dimension-in-julia-size-method/
-# https://www.juliawiki.com/wiki/Matrices_in_Julia
-# https://www.juliasos.com/data/the-best-way-to-convert-vector-into-matrix-in-julia/
-# https://www.mathsisfun.com/algebra/matrix-multiplying.html
-# https://docs.julialang.org/en/v1/base/base/#Base.error
-
 function matmulisvalid(n1::Int, n2::Int)::Bool
     return n1 == n2
 end
---
+++ sources.md
@@ -0,0 +1,7 @@
+# Matmul.jl
+Sources:
+https://www.geeksforgeeks.org/get-array-dimensions-and-size-of-a-dimension-in-julia-size-method/
+https://www.juliawiki.com/wiki/Matrices_in_Julia
+https://www.juliasos.com/data/the-best-way-to-convert-vector-into-matrix-in-julia/
+https://www.mathsisfun.com/algebra/matrix-multiplying.html
+https://docs.julialang.org/en/v1/base/base/#Base.error
