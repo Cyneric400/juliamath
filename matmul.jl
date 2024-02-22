a4bb3bd5cb667b0d3414e8af35761cb40a226fc3
Cyneric400 <davidrsharpe@southern.edu>
2024-02-22 09:07:19 -0500

    gitignore, matmul tweaks, updating, and old code deletion

--- matmul.jl
+++ matmul.jl
@@ -1,16 +1,12 @@
 # First started in MATH-280 class, mid-Feb 2024
 # Getting loops together: 2/20/2024
-
-function matmulisvalid(n1::Int, n2::Int)::Bool
-    return n1 == n2
-end
-
+# Finished first draft: 2/22/2024
 
 function matmul(A::Matrix{Int64}, B::Matrix{Int64})::Matrix{Int64}
     m = size(A)[1]
     n = size(B)[1]
     p = size(B)[2]
-    if !(matmulisvalid(size(A)[2], n))
+    if size(A)[2]!=n
         throw(error("Invalid matrix sizes"))
     end
     result = zeros(m,p)
@@ -18,9 +14,6 @@ function matmul(A::Matrix{Int64}, B::Matrix{Int64})::Matrix{Int64}
         for j in 1:p
             for k in 1:n
                 result[i,j] += A[i,k] * B[k,j]
-#                 @show i, j, k
-#                 @show A[i,k]
-#                 @show B[k,j]
             end
         end
     end
@@ -28,9 +21,7 @@ function matmul(A::Matrix{Int64}, B::Matrix{Int64})::Matrix{Int64}
 end
 
 A = [1 0; 3 2; -5 1;]
-B = [6 1 3 4; 3 2 3 4; 10 23 423 1]
-# B = reshape(B, 2, 1)
-# A = [1 2; 3 4]
-# B = [5 6; 7 8]
+B = [6 1;]
+B = reshape(B, 2, 1)
 
 @show matmul(A,B)
