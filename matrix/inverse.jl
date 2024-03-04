9b096a75670d271103ba9d7cce115c8968cf5bde
Cyneric400 <davidrsharpe@southern.edu>
2024-03-04 18:57:57 -0500

    functioning? inverse code

--- matrix/inverse.jl
+++ matrix/inverse.jl
@@ -1,9 +1,18 @@
+# Create a fraction class based off of CPTR124 code to be able to write these out effectively
+struct Fraction
+end
+
+
+
 function inverse(m::Matrix{Int64})
     if size(m) != (2,2)
         throw(error("not defined"))
     else
         det = m[1]*m[4]-(m[2]*m[3])
-#         return (1/det)*m
+        if det != 0
+            swappedmat = [m[4] -m[2]; -m[3] m[1]]
+            return swappedmat * (1/det)
+        end
     end
 end
 
